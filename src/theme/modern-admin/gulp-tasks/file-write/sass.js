7ad80ba3b3e151e617a0ca82134c27d6a0835204
Juan Carlos Vega Neira <jcvegan@live.com>
2019-12-12 06:51:25 -0500

    Adding resources

--- src/theme/modern-admin/gulp-tasks/file-write/sass.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-module.exports = function(gulp, callback) {
-   if(material == 'material-'){
-      return fs.writeFile(config.source.sass + '/core/variables/_variables.scss', "//Auto generated file for material layouts. \n @import \"_material-variables\";", callback);
-   }else{
-      return fs.writeFile(config.source.sass + '/core/variables/_variables.scss', "//Auto generated file for default layouts. \n @import \"_bootstrap-variables\";", callback);
-   }
-};
